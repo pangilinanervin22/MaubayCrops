635268271c13d60ea53b9c0396df509fcd91e430
Jiseeeh <johncarlo.camara1@gmail.com>
2024-05-31 12:14:53 +0800

    feat: add homepage

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+interface HeroProps {}
+
+const Hero: React.FC<HeroProps> = ({}) => {
+  return (
+    <section>
+      <div className="bg-hero bg-cover bg-center h-hero bg-no-repeat flex items-center justify-center">
+        <section className="h-full p-5 flex flex-col items-center space-y-6 lg:space-y-16">
+          <h1 className="text-center text-dark-brown text-3xl font-bold lg:text-5xl ">
+            One Stop Solution for your farming needs
+          </h1>
+          <h2 className="text-center text-dark-blue text-2xl font-bold lg:text-4xl">
+            Buy Fertilizers, Insectisides, Seeds & other Agriculture Products
+          </h2>
+          <button className="bg-gradient-button px-6 py-4 text-center transition duration-500 bg-[length:200%_auto] hover:border-white hover:border-2 text-white shadow-button shadow-white rounded-md whitespace-nowrap border-2 border-primary w-40 text-xl">
+            SHOP NOW
+          </button>
+        </section>
+      </div>
+    </section>
+  );
+};
+
+export { Hero };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import { Hero } from "./components/Hero";
+import { Featured } from "./components/Featured";
+import { Navbar } from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className="min-h-screen">
+      <Navbar />
+      <article>
+        <Hero />
+        <Featured />
+      </article>
     </main>
   );
 }
