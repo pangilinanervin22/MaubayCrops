717f9a6f755eddcf0d7aedadcc530cd6e2ba32ad
Jiseeeh <johncarlo.camara1@gmail.com>
2024-05-31 18:52:42 +0800

    feat: push to products on click shop

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 interface HeroProps {}
 
 const Hero: React.FC<HeroProps> = ({}) => {
+  const router = useRouter();
   return (
     <section>
       <div className="bg-hero bg-cover bg-center h-hero bg-no-repeat flex items-center justify-center">
@@ -13,7 +16,12 @@ const Hero: React.FC<HeroProps> = ({}) => {
           <h2 className="text-center text-dark-blue text-2xl font-bold lg:text-4xl">
             Buy Fertilizers, Insectisides, Seeds & other Agriculture Products
           </h2>
-          <button className="bg-gradient-button px-6 py-4 text-center transition duration-500 bg-[length:200%_auto] hover:border-white hover:border-2 text-white shadow-button shadow-white rounded-md whitespace-nowrap border-2 border-primary w-40 text-xl">
+          <button
+            className="bg-gradient-button px-6 py-4 text-center transition duration-500 bg-[length:200%_auto] hover:border-white hover:border-2 text-white shadow-button shadow-white rounded-md whitespace-nowrap border-2 border-primary w-40 text-xl"
+            onClick={() => {
+              router.push("/products");
+            }}
+          >
             SHOP NOW
           </button>
         </section>
