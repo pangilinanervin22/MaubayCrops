4286009ad6edddf5e82b5ed165f36879567c3aeb
Jiseeeh <johncarlo.camara1@gmail.com>
2024-05-31 18:53:55 +0800

    feat: add product class

---
+++ src/app/interfaces/Product.ts
@@ -0,0 +1,69 @@
+export class Product implements IProduct {
+  _id: string;
+  categoryName: string;
+  description: string;
+  imgUrl: string;
+  price: number;
+  productDetails: IProductDetails;
+  rating: number;
+  title: string;
+
+  constructor(
+    id: string,
+    categoryName: string,
+    description: string,
+    imgUrl: string,
+    price: number,
+    productDetails: IProductDetails,
+    rating: number,
+    title: string
+  ) {
+    this._id = id;
+    this.categoryName = categoryName;
+    this.description = description;
+    this.imgUrl = imgUrl;
+    this.price = price;
+    this.productDetails = productDetails;
+    this.rating = rating;
+    this.title = title;
+  }
+
+  static fromFirestore(id: string, data: any): Product {
+    const {
+      categoryName,
+      description,
+      imgUrl,
+      price,
+      productDetails,
+      rating,
+      title,
+    } = data;
+    return new Product(
+      id,
+      categoryName,
+      description,
+      imgUrl,
+      price,
+      productDetails,
+      rating,
+      title
+    );
+  }
+}
+
+export interface IProduct {
+  _id: string;
+  categoryName: string;
+  description: string;
+  imgUrl: string;
+  price: number;
+  productDetails: IProductDetails;
+  rating: number;
+  title: string;
+}
+
+interface IProductDetails {
+  Brand: string;
+  Color: string;
+  Material: string;
+}
