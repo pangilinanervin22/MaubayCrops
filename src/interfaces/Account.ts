7e6c6d6ed21cc006295417afa6bba6ce6423fa50
Ervin Pangilinan <pangilinanervin22@gmail.com>
2024-06-02 00:37:10 +0800

    refactor: Update Account and Cart interfaces and related types

--- src/interfaces/Account.ts
+++ src/interfaces/Account.ts
@@ -1,32 +1,27 @@
 import { Product } from "./Product";
 
 export default interface Account {
-  _id?: string; // Firestore automatically generates this
-  uid?: string; // For reference to authentication of Firebase
-  password?: string; // For reference only, do not store in Firestore
-  name: string;
-  email: string;
-  userType: "Farmer" | "Seller" | "Admin";
-  address: Address[];
+    _id?: string; // Firestore automatically generates this
+    uid?: string; // For reference to authentication of Firebase
+    password?: string; // For reference only, do not store in Firestore
+    name: string;
+    email: string;
+    userType: "Farmer" | "Seller" | "Admin";
+    address: Address[];
 }
 
 export interface Address {
-  _id?: string;
-  nameReceiver: string;
-  province: string;
-  city: string;
-  barangay: string;
-  building: string;
-  phone: string;
+    _id?: string;
+    nameReceiver: string;
+    province: string;
+    city: string;
+    barangay: string;
+    building: string;
+    phone: string;
 }
 
-export interface Cart {
-  userId: string;
-  _id?: string;
-  product: CartProduct[];
+export interface CartItem extends Product {
+    quantity: number;
+    productId: string;
 }
 
-interface CartProduct {
-  productId: string;
-  quantity: number;
-}
