d5e78c5fadc19f58e67491c44ea3c8d480893d52
Jiseeeh <johncarlo.camara1@gmail.com>
2024-06-02 00:25:22 +0800

    refactor: remove Navbar component from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,10 @@
 
 import { Hero } from "../components/Hero";
 import { Featured } from "../components/Featured";
-import { Navbar } from "../components/Navbar";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <Navbar />
       <article>
         <Hero />
         <Featured />
