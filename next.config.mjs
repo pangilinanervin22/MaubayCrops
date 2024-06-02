31648db421144b5da84e9834f74ea132c6dfb089
Jiseeeh <johncarlo.camara1@gmail.com>
2024-06-03 00:07:51 +0800

    feat: Add image domains for lazy loading in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "m.media-amazon.com",
+      "images-eu.ssl-images-amazon.com",
+      "static.vecteezy.com",
+    ],
+  },
+};
 
 export default nextConfig;
