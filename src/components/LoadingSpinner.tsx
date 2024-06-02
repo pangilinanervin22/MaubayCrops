9d74126cb575e764b26971c513ec73e2002d752b
Jiseeeh <johncarlo.camara1@gmail.com>
2024-06-02 17:40:28 +0800

    feat: Add LoadingSpinner component for displaying a loading animation

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+interface LoadingSpinnerProps {}
+
+const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({}) => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-green-500"></div>
+    </div>
+  );
+};
+
+export { LoadingSpinner };
