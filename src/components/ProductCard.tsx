e8ef70dbfba374a6a64fb47c0132ae4ff063ed52
Jiseeeh <johncarlo.camara1@gmail.com>
2024-06-04 03:15:36 +0800

    refactor: Disable add to cart button for out of stock items in ProductCard component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -79,6 +79,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
         ) : (
           <button
             className="mt-auto btn-blue flex justify-center w-full py-2"
+            disabled={isOutOfStock}
             onClick={() => {
               addToCart(product);
             }}
